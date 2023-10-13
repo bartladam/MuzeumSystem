1772cdee5e88f58dcc70bdba88a42c6b86fe49c5
Adam46072 <adam.bartl2001@seznam.cz>
2023-10-13 13:12:41 +0200

    exhibition is almost done, without connection

--- MuzeumSystem/Artwork.cs
+++ MuzeumSystem/Artwork.cs
@@ -8,5 +8,17 @@ namespace MuzeumSystem
 {
     internal class Artwork
     {
+        public string nameArtwork { get; private set; }
+        public string descriptionArtWork { get; private set; }
+        public DateTime createdArtWork { get; private set; }
+        public string Artist { get; private set; }
+        public Artwork(string nameArtwork, string descriptionArtWork, DateTime createdArtWork, string Artist)
+        {
+            this.nameArtwork = nameArtwork;
+            this.descriptionArtWork = descriptionArtWork;
+            this.createdArtWork = createdArtWork;
+            this.Artist = Artist;
+
+        }
     }
 }
--- MuzeumSystem/Exhibition.cs
+++ MuzeumSystem/Exhibition.cs
@@ -8,5 +8,16 @@ namespace MuzeumSystem
 {
     internal class Exhibition
     {
+        private List<Artwork> exhibitionArtworks { get; set; }
+        public Guide guide { get; private set; }
+        public Exhibition(Guide guide,params Artwork[] artworks)
+        {
+            exhibitionArtworks = new List<Artwork>(artworks);
+            this.guide = guide;
+        }
+        public void ShowExhibition()
+        {
+
+        }
     }
 }
